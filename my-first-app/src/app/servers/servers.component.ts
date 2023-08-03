3b2800c80c40f56bc55265ba14b4688b70bb3ad1
shaown224 <60014974+shaown224@users.noreply.github.com>
2023-08-03 07:57:05 +0600

    Assignment 2

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { timeInterval, timeout } from 'rxjs';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit{
+
+  addNewServer = false;
+  serverCreationStatus = 'There was no server Created';
+  serverName = '';
+  userName = '';
+  resetButton= false;
+  
+
+  constructor(){
+
+    // setTimeout(() => {
+    //   this.addNewServer = true;
+    // }, 2000);
+  
+  }
+ 
+  ngOnInit(){
+      
+  }
+
+  // createServer(){
+
+  //   this.serverCreationStatus= 'Server Created Successfully';
+
+  // }
+  // onUpdateServerName(event:any){
+
+  //   this.serverName = (<HTMLInputElement>event.target).value;
+
+  // }
+
+  resetButtonClickable(event:any){
+
+    this.userName = (<HTMLInputElement>event.target).value;
+
+    if(this.userName.length !== 0){
+      this.resetButton = true;
+    }
+  }
+
 
 }
